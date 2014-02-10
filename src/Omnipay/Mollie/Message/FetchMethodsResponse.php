4948a0f6ac0205f3404687a0c7e0f3a9d5cc39e1
nettob <nettob@users.noreply.github.com>
2014-02-10 19:01:36 +0100

    ps fix

--- src/Omnipay/Mollie/Message/FetchMethodsResponse.php
+++ src/Omnipay/Mollie/Message/FetchMethodsResponse.php
@@ -2,14 +2,16 @@
 
 namespace Omnipay\Mollie\Message;
 
-class FetchMethodsResponse extends AbstractResponse {
+class FetchMethodsResponse extends AbstractResponse
+{
 
     /**
      * Return available issuers as an associative array.
      *
      * @return array|null
      */
-    public function getMethods() {
+    public function getMethods()
+	{
         if (isset($this->data['data'])) {
             $result = array();
             foreach ($this->data['data'] as $method) {
