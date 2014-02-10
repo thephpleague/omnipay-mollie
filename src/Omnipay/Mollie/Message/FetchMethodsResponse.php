fa8f615752cfae9550d2549e4a6d13a77ae779b1
nettob <nettob@users.noreply.github.com>
2014-02-10 18:44:37 +0100

    PR fixes
    travic-ci errors fixed

--- src/Omnipay/Mollie/Message/FetchMethodsResponse.php
+++ src/Omnipay/Mollie/Message/FetchMethodsResponse.php
@@ -2,30 +2,29 @@
 
 namespace Omnipay\Mollie\Message;
 
-class FetchMethodsResponse extends AbstractResponse
-{
+class FetchMethodsResponse extends AbstractResponse {
+
     /**
      * Return available issuers as an associative array.
      *
      * @return array|null
      */
-    public function getMethods()
-    {
+    public function getMethods() {
         if (isset($this->data['data'])) {
-	        $result = array();
+            $result = array();
             foreach ($this->data['data'] as $method) {
                 $result[] = array(
-                    'id'     => $method['id'],
-                    'name'   => $method['description'],
+                    'id' => $method['id'],
+                    'name' => $method['description'],
                     'amount' => array(
-                    	'min' => $method['amount']['minimum'],
-                    	'max' => $method['amount']['maximum']
+                        'min' => $method['amount']['minimum'],
+                        'max' => $method['amount']['maximum']
                     )
                 );
             }
             return $result;
         }
-
         return null;
     }
+
 }
