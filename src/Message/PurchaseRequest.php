fdb31eedabb7b4cbb973c434a1a53a196bd07799
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2017-07-31 20:34:21 +0200

    Add the 'locale' and 'billingEmail' parameters

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -20,6 +20,26 @@ class PurchaseRequest extends AbstractRequest
         return $this->setParameter('metadata', $value);
     }
 
+    public function getLocale()
+    {
+        return $this->getParameter('locale');
+    }
+
+    public function setLocale($value)
+    {
+        return $this->setParameter('locale', $value);
+    }
+
+    public function getBillingEmail()
+    {
+        return $this->getParameter('billingEmail');
+    }
+
+    public function setBillingEmail($value)
+    {
+        return $this->setParameter('billingEmail', $value);
+    }
+
     public function getData()
     {
         $this->validate('apiKey', 'amount', 'description', 'returnUrl');
@@ -30,17 +50,26 @@ class PurchaseRequest extends AbstractRequest
         $data['redirectUrl'] = $this->getReturnUrl();
         $data['method']      = $this->getPaymentMethod();
         $data['metadata']    = $this->getMetadata();
+
         if ($this->getTransactionId()) {
             $data['metadata']['transactionId'] = $this->getTransactionId();
         }
-        $issuer = $this->getIssuer();
-        if ($issuer) {
+
+        if ($issuer = $this->getIssuer()) {
             $data['issuer'] = $issuer;
         }
 
         $webhookUrl = $this->getNotifyUrl();
         if (null !== $webhookUrl) {
             $data['webhookUrl'] = $webhookUrl;
+        }
+
+        if ($locale = $this->getLocale()) {
+            $data['locale'] = $locale;
+        }
+
+        if ($billingEmail = $this->getBillingEmail()) {
+            $data['billingEmail'] = $billingEmail;
         }
 
         return $data;
