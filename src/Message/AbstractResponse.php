7ff3931f044d01b2e4fcb1fd763f0f2cab2f90d0
Martijn Smit <smit@mollie.com>
2018-07-19 14:54:53 +0200

    Update complete purchase request tests

--- src/Message/AbstractResponse.php
+++ src/Message/AbstractResponse.php
@@ -6,7 +6,11 @@ class AbstractResponse extends \Omnipay\Common\Message\AbstractResponse
 {
     public function isSuccessful()
     {
-        return !isset($this->data['status']) || ($this->data['status'] >= 200 && $this->data['status'] < 300);
+        if(isset($this->data['status']) && isset($this->data['detail'])) {
+            return $this->data['status'] >= 200 && $this->data['status'] < 300;
+        }
+
+        return true;
     }
 
     public function getMessage()
